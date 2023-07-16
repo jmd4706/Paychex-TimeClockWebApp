45c68611ff86e7a590ae4a5e51e899fdfbd79e3a
JordanDetwiler <jmd4706@rit.edu>
2023-07-16 01:44:40 -0400

    Added an error page for when an employee cannot be found in the database, and also added the preliminary clock in/out page.

--- src/main/java/com/example/timeclock/TimeClockController.java
+++ src/main/java/com/example/timeclock/TimeClockController.java
@@ -1,6 +1,7 @@
 package com.example.timeclock;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +21,32 @@ public class TimeClockController {
         return modelAndView;
     }
 
-    @RequestMapping("/users/{id}")
-    public ModelAndView clockPage(@PathVariable String id) {
-        ModelAndView modelAndView = new ModelAndView();
+    @RequestMapping("/users/{id}/searchForUser")
+    public ModelAndView searchForUser(@PathVariable String id) {
+        ModelAndView modelAndView;
         Optional<Employee> query = dataHandler.findById(id);
-        if(query.isPresent()) {
+        if(query.isEmpty()) {
+            modelAndView = new ModelAndView("redirect:/error/EmployeeNotFound");
+        } else {
             Employee employee = query.get();
+            modelAndView = new ModelAndView("redirect:/users/" + id + "/clock");
             System.out.println(employee);
         }
 
+        return modelAndView;
+    }
+
+    @RequestMapping("/error/EmployeeNotFound")
+    public ModelAndView employeeNotFoundPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("EmployeeNotFound.html");
+        return modelAndView;
+    }
+
+    @RequestMapping("/users/{id}/clock")
+    public ModelAndView clockPage(@PathVariable String id) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("../../clock.html");
         return modelAndView;
     }
 
--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -1,4 +1,4 @@
 function submitButton() {
     let employeeID = document.getElementById("idInput").value;
-    window.location.href = window.location.href + "users/" + employeeID;
+    window.location.href = window.location.href + "users/" + employeeID + "/searchForUser";
 }
